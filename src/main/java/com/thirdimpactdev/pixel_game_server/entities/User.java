5b6cdb5c7904e763ffef1829f48b7ebb49af1245
xOnlinEx <daniel.floresvargas666@gmail.com>
2024-10-08 20:52:19 -0400

    add: new base for project
    v0.0.0

---
+++ src/main/java/com/thirdimpactdev/pixel_game_server/entities/User.java
@@ -0,0 +1,25 @@
+package com.thirdimpactdev.pixel_game_server.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+}
