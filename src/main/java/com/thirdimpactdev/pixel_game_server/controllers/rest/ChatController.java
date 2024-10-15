0cdf40edf6fee6b7483048a237cd6ee1990713ec
JuanCJ9688 <ironjohn788@gmail.com>
2024-10-15 11:00:38 -0500

    Added functionality to pixel rendering.

--- src/main/java/com/thirdimpactdev/pixel_game_server/controllers/rest/ChatController.java
+++ src/main/java/com/thirdimpactdev/pixel_game_server/controllers/rest/ChatController.java
@@ -1,5 +1,8 @@
 package com.thirdimpactdev.pixel_game_server.controllers.rest;
 
+import com.thirdimpactdev.pixel_game_server.models.Grid;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
@@ -10,17 +13,23 @@ import com.thirdimpactdev.pixel_game_server.models.Pixel;
 @Controller
 public class ChatController {
 
+    @Autowired
+   private Grid grilla;
+
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
-    public Pixel sendMessage(Pixel pixel) {
-        return pixel;
+    public Grid sendMessage(Pixel pixel) {
+        grilla.setValue(pixel);
+        return grilla;
     }
 
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
-    public Pixel addUser(Pixel pixel, SimpMessageHeaderAccessor headerAccessor) {
+    public Grid addUser(Pixel pixel, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("pixel", pixel.getX());
-        return pixel;
+
+        grilla.setValue(pixel);
+        return grilla;
     }
 
 }
