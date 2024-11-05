eff22f7a74ea4924bdab6eb55aaa045a47a61b03
JuanCJ9688 <ironjohn788@gmail.com>
2024-11-04 19:55:18 -0500

    Added CorsConfig.

---
+++ server/src/main/java/com/thirdimpactdev/pixel_game_server/configs/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.thirdimpactdev.pixel_game_server.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173","http://localhost:5432","http://localhost:8080","http://localhost:8081")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
